d807a693eafdc6218d2b960b07d11f7b85a64295
Joa-Akm <148182470+Joa-Akm@users.noreply.github.com>
2024-02-08 16:41:15 +0400

    lesson1.js file

---
+++ lessons1.js
@@ -0,0 +1,93 @@
+let lessons =[
+
+    {
+        "id": 1001,
+        "title": "Mathematics",
+        "location":"Hendon",
+        "price": 175,
+        "image": "images/maths.png",
+        "availableSpaces": 15
+       
+      }, 
+      {
+        "id": 1002,
+        "title": "Physics",
+        "location":"Dubai",
+        "price": 150,
+        "image": "images/physics.png",
+        "availableSpaces": 15
+       
+      }, 
+      {
+        "id": 1003,
+        "title": "Literature",
+        "location":"Liverpool",
+        "price": 100,
+        "image": "images/literature.png",
+        "availableSpaces": 5
+       
+      }, 
+      {
+        "id": 1004,
+        "title": "English",
+        "location":"Bristol",
+        "price": 120,
+        "image": "images/english.png",
+        "availableSpaces": 10
+       
+      }, 
+      {
+        "id": 1005,
+        "title": "ICT",
+        "location":"Tokyo",
+        "price": 180,
+        "image": "images/ict.png",
+        "availableSpaces": 10
+       
+      }, 
+      {
+        "id": 1006,
+        "title": "Chemistry",
+        "location":"Sharjah",
+        "price": 150,
+        "image": "images/chemistry.png",
+        "availableSpaces": 15
+       
+      }, 
+      {
+        "id": 1007,
+        "title": "Entrepreneurship",
+        "location":"London",
+        "price": 90,
+        "image": "images/entrepreneurship.png",
+        "availableSpaces": 8
+       
+      }, 
+      {
+        "id": 1008,
+        "title": "History",
+        "location":"Dubai",
+        "price": 130,
+        "image": "images/history.png",
+        "availableSpaces": 10
+       
+      }, 
+      {
+        "id": 1009,
+        "title": "Geography",
+        "location":"London",
+        "price": 100,
+        "image": "images/geography.png",
+        "availableSpaces": 15
+       
+      }, 
+      {
+        "id": 1010,
+        "title": "Biology",
+        "location":"Hendon",
+        "price": 150,
+        "image": "images/biology.png",
+        "availableSpaces": 10
+       
+      }, 
+]
